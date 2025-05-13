7ae06098bb6deb650e0b740c389cafed4843f768
varunbala2237 <varunbalasurya4444@gmail.com>
2025-05-14 01:15:39 +0530

    fix: Add import statements for Firebase authentication in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+// Header.js
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from "react-router-dom";
 import { auth, signOutWithAccount } from "../firebase/firebase-auth";
