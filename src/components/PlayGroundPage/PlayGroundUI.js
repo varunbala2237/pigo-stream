f3f0e6ff826c569913db3302b70287c5d4caac8c
varunbala2237 <varunbalasurya4444@gmail.com>
2025-06-17 14:54:20 +0530

    refactor: streamline loading and error handling in PlayGround component

--- src/components/PlayGroundPage/PlayGroundUI.js
+++ src/components/PlayGroundPage/PlayGroundUI.js
@@ -28,53 +28,52 @@ function PlayGround() {
     });
   }, []);
 
-  // Handling loading state and error state
-  if (!mediaInfo) {
-    return isError ? (
-      <ConnectionModal show={isError} />
-    ) : (
-      <OverlaySpinner visible={isLoading} />
-    );
-  }
-
   // Decide which GridComponent to render
   const GridComponent = type === 'movie' ? MovieGrid : TvGrid;
 
   return (
     <div className="index-page">
-      <Header />
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: backgroundImage
-              ? `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)), url(${backgroundImage})`
-              : 'none',
-          }}
-        >
-          {/* Dark Overlay */}
-          <div
-            className="position-absolute w-100 h-100"
-            style={{
-              top: 0,
-              left: 0,
-              background: 'rgba(0, 0, 0, 0.4)',
-              zIndex: 1,
-            }}
-          ></div>
-        </div>
+      {/* Overlay spinner for loading state */}
+      <OverlaySpinner visible={isLoading} />
 
-        {/* Grid Components */}
+      <Header />
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: backgroundImage
+            ? `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)), url(${backgroundImage})`
+            : 'none',
+        }}
+      >
+        {/* Dark Overlay */}
+        <div
+          className="position-absolute w-100 h-100"
+          style={{
+            top: 0,
+            left: 0,
+            background: 'rgba(0, 0, 0, 0.4)',
+            zIndex: 1,
+          }}
+        ></div>
+      </div>
+
+      {/* Grid Components */}
+      {mediaInfo != null &&
         <GridComponent
           id={id}
           type={type}
           mediaInfo={mediaInfo}
           setBackgroundImage={setBackgroundImage}
         />
+      }
 
-        {/* Footer Backspace & Footer */}
-        <div className="divider" style={{ height: '6rem' }}></div>
-        <Footer />
-      </div>
+      {/* Connection Modal */}
+      {isError && <ConnectionModal show={isError} />}
+
+      {/* Footer Backspace & Footer */}
+      <div className="divider" style={{ height: '6rem' }}></div>
+      <Footer />
+    </div>
   );
 }
 
