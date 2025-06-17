6d478cced75a477d59ca4cadceecaa9a23008f4b
varunbala2237 <varunbalasurya4444@gmail.com>
2025-06-17 23:38:12 +0530

    refactor: update placeholder image URL for episodes in EpisodeSection component

--- src/components/PlayGroundPage/Sections/EpisodeSection.js
+++ src/components/PlayGroundPage/Sections/EpisodeSection.js
@@ -39,7 +39,7 @@ function EpisodeSection({
                   src={
                     episode.still_path
                       ? `https://image.tmdb.org/t/p/w185${episode.still_path}`
-                      : ''
+                      : 'https://placehold.co/600x400'
                   }
                   className="custom-theme-radius-top-low"
                   alt="empty"
