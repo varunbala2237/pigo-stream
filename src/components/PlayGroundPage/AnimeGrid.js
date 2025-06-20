6b9c9ebeeff619c8b2e00f8fc4419e92d385bb22
varunbala2237 <varunbalasurya4444@gmail.com>
2025-06-20 22:52:16 +0530

    refactor: remove mediaInfo from AnimeGrid component for cleaner data display

--- src/components/PlayGroundPage/AnimeGrid.js
+++ src/components/PlayGroundPage/AnimeGrid.js
@@ -1,10 +1,10 @@
 // AnimeGrid.js
-function AnimeGrid({ animeInfo, mediaInfo }) {
+function AnimeGrid({ animeInfo }) {
   return (
     <div style={{ color: 'white', padding: '1rem' }}>
       <h2>Anime Data (Matched)</h2>
       <pre style={{ whiteSpace: 'pre-wrap' }}>
-        {JSON.stringify({ mediaInfo, animeInfo }, null, 2)}
+        {JSON.stringify({ animeInfo }, null, 2)}
       </pre>
     </div>
   );
