ba7e070d7fd4821d69f318baadf8d14ad1523ffe
varunbala2237 <varunbalasurya4444@gmail.com>
2025-06-20 23:07:13 +0530

    refactor: update routing implementation to use createBrowserRouter and RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // App.js
-import React from 'react';
-import './App.css'
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import AuthUI from './components/AuthPage/AuthUI';
 import IndexUI from './components/IndexPage/IndexUI';
 import PlayGroundUI from './components/PlayGroundPage/PlayGroundUI';
@@ -9,19 +12,25 @@ import WatchHistoryUI from './components/WatchHistoryPage/WatchHistoryUI';
 import MyListUI from './components/MyListPage/MyListUI';
 import PigoStoreUI from './components/PigoStorePage/PigoStoreUI';
 
+const router = createBrowserRouter(
+  [
+    { path: '/', element: <AuthUI /> },
+    { path: '/index', element: <IndexUI /> },
+    { path: '/play', element: <PlayGroundUI /> },
+    { path: '/watch-history', element: <WatchHistoryUI /> },
+    { path: '/my-list', element: <MyListUI /> },
+    { path: '/pigostore', element: <PigoStoreUI /> },
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+    },
+  }
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<AuthUI/>} />
-            <Route path="/index" element={<IndexUI/>} />
-            <Route path="/play" element={<PlayGroundUI/>} />
-            <Route path="/watch-history" element={<WatchHistoryUI/>} />
-            <Route path="/my-list" element={<MyListUI/>} />
-            <Route path="/pigostore" element={<PigoStoreUI />} />
-        </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
