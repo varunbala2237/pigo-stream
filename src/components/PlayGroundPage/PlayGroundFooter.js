f63b073512b014c4de72e5babafb030f783ef172
varunbala2237 <varunbalasurya4444@gmail.com>
2025-06-24 05:47:03 +0530

    fix: update play button icon for consistency

--- src/components/PlayGroundPage/PlayGroundFooter.js
+++ src/components/PlayGroundPage/PlayGroundFooter.js
@@ -105,7 +105,7 @@ function PlayGroundFooter({ id, type, mediaURL }) {
                             aria-label="Play"
                             title="Play"
                         >
-                            <i className="bi bi-play-fill theme-color me-2"></i>
+                            <i className="bi bi-play-circle theme-color me-2"></i>
                             <span className="text-white dynamic-ss">Play</span>
                         </button>
                     </li>
