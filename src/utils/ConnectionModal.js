d5e5a9580e7916467bc48bc6e22ed22dd4d2e83f
varunbala2237 <varunbalasurya4444@gmail.com>
2025-07-07 15:49:35 +0530

    fix: correct punctuation in error message for improved clarity

--- src/utils/ConnectionModal.js
+++ src/utils/ConnectionModal.js
@@ -18,7 +18,7 @@ function ConnectionModal({ show }) {
             <div ref={modalRef} className="modal-content dynamic-fs bd-callout-dark custom-theme-radius-low text-white border-0">
               <div className="modal-body justify-content-center text-center border-0">
                 <i className="bi bi-x-circle text-danger dynamic-hs"></i>
-                <p className="mt-3 dynamic-fs">Server isn't responding right now. Please try again shortly.</p>
+                <p className="mt-3 dynamic-fs">Server isn't responding right now, Please try again shortly.</p>
                 <button
                   className="btn btn-primary border-0 rounded-pill btn-md d-none d-md-inline-block mt-3"
                   onClick={() => window.location.reload()}
