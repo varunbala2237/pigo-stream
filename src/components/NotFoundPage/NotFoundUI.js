98df0193c2ad0b5d009d8f6e9a9321d96d347df1
varunbala2237 <varunbalasurya4444@gmail.com>
2025-07-15 05:16:52 +0530

    v0.2.2 Final

--- src/components/NotFoundPage/NotFoundUI.js
+++ src/components/NotFoundPage/NotFoundUI.js
@@ -3,7 +3,7 @@
 const NotFoundUI = () => {
   return (
     <div className="container-fluid d-flex flex-column justify-content-center align-items-center vh-100 p-0">
-      <div className="w-100 text-center text-white">
+      <div className="w-100 d-flex flex-column text-center text-white">
         <span className="dynamic-hs fw-bold">404 - Page Not Found</span>
         <span className="dynamic-fs">The page you're looking for doesn't exist.</span>
       </div>
